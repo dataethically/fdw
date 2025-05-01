c93bcd1277158f50ea22b281d962a5dfb0aef52a
dataethically <97569537+dataethically@users.noreply.github.com>
2025-05-01 00:16:26 -0700

    Update fdw_run.sh

--- fdw_run.sh
+++ fdw_run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Replace environment variables in the SQL file
-envsubst < fdw_setup.sql > fdw_setup_env.sql
-
 # Run the SQL script
 echo "Executing FDW setup..."
 psql $DATABASE_URL -f fdw_setup_env.sql
